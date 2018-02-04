c8211e65787bf608ab71cd847066c0e4edaa48cb
dang93 <dguerrero141@gmail.com>
2018-02-03 21:40:32 -0800

    hello world DG

--- StudyMeet/GeneratedFiles/ui_studymeet.h
+++ StudyMeet/GeneratedFiles/ui_studymeet.h
@@ -13,11 +13,17 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,30 +31,118 @@ QT_BEGIN_NAMESPACE
 class Ui_StudyMeetClass
 {
 public:
+    QWidget *centralWidget;
+    QWidget *layoutWidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout_2;
+    QSpacerItem *horizontalSpacer_2;
+    QGridLayout *gridLayout_2;
+    QLabel *label;
+    QPushButton *heyButton;
+    QSpacerItem *verticalSpacer;
+    QWidget *layoutWidget1;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *exitButton;
+    QSpacerItem *verticalSpacer_2;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *StudyMeetClass)
     {
         if (StudyMeetClass->objectName().isEmpty())
             StudyMeetClass->setObjectName(QStringLiteral("StudyMeetClass"));
-        StudyMeetClass->resize(600, 400);
+        StudyMeetClass->resize(582, 590);
+        centralWidget = new QWidget(StudyMeetClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        layoutWidget = new QWidget(centralWidget);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(0, 70, 185, 308));
+        verticalLayout = new QVBoxLayout(layoutWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        horizontalSpacer_2 = new QSpacerItem(98, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer_2);
+
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        label = new QLabel(layoutWidget);
+        label->setObjectName(QStringLiteral("label"));
+        QFont font;
+        font.setPointSize(24);
+        label->setFont(font);
+        label->setTextFormat(Qt::PlainText);
+
+        gridLayout_2->addWidget(label, 0, 0, 1, 1);
+
+        heyButton = new QPushButton(layoutWidget);
+        heyButton->setObjectName(QStringLiteral("heyButton"));
+
+        gridLayout_2->addWidget(heyButton, 1, 0, 1, 1);
+
+
+        horizontalLayout_2->addLayout(gridLayout_2);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        verticalSpacer = new QSpacerItem(20, 228, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        layoutWidget1 = new QWidget(centralWidget);
+        layoutWidget1->setObjectName(QStringLiteral("layoutWidget1"));
+        layoutWidget1->setGeometry(QRect(-30, 0, 601, 71));
+        verticalLayout_2 = new QVBoxLayout(layoutWidget1);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalSpacer = new QSpacerItem(418, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        exitButton = new QPushButton(layoutWidget1);
+        exitButton->setObjectName(QStringLiteral("exitButton"));
+
+        horizontalLayout->addWidget(exitButton);
+
+
+        verticalLayout_2->addLayout(horizontalLayout);
+
+        verticalSpacer_2 = new QSpacerItem(20, 348, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer_2);
+
+        StudyMeetClass->setCentralWidget(centralWidget);
+        layoutWidget->raise();
+        layoutWidget->raise();
+        heyButton->raise();
         menuBar = new QMenuBar(StudyMeetClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 582, 21));
         StudyMeetClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(StudyMeetClass);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        StudyMeetClass->addToolBar(mainToolBar);
-        centralWidget = new QWidget(StudyMeetClass);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        StudyMeetClass->setCentralWidget(centralWidget);
+        StudyMeetClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(StudyMeetClass);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         StudyMeetClass->setStatusBar(statusBar);
 
         retranslateUi(StudyMeetClass);
+        QObject::connect(exitButton, SIGNAL(clicked()), StudyMeetClass, SLOT(close()));
 
         QMetaObject::connectSlotsByName(StudyMeetClass);
     } // setupUi
@@ -56,6 +150,9 @@ public:
     void retranslateUi(QMainWindow *StudyMeetClass)
     {
         StudyMeetClass->setWindowTitle(QApplication::translate("StudyMeetClass", "StudyMeet", 0));
+        label->setText(QApplication::translate("StudyMeetClass", "Hi", 0));
+        heyButton->setText(QApplication::translate("StudyMeetClass", "Hey", 0));
+        exitButton->setText(QApplication::translate("StudyMeetClass", "exit", 0));
     } // retranslateUi
 
 };
