78b74f192d349618925ff2a65cfeccfe51b5859a
Xela <alexapilado@live.com>
2018-03-12 16:12:02 -0700

    implemented Account class (partially)

--- StudyMeet/Account.cpp
+++ StudyMeet/Account.cpp
@@ -1,9 +1,37 @@
 #include "Account.h"
 
 
-
 Account::Account()
 {
+	username = password = " ";
+}
+
+Account::Account(string name, string pword)
+{
+	username = name;
+	password = pword;
+}
+
+string Account::get_username()
+{
+	return username;
+}
+
+string Account::get_password()
+{
+	return password;
+}
+
+int Account::isUsernameAvailable(string name)
+{
+	//check DB for username I assume
+	return 0;
+}
+
+void Account::createAccount(string name, string pword)
+{
+	username = name;
+	password = pword;
 }
 
 
--- StudyMeet/Account.h
+++ StudyMeet/Account.h
@@ -1,11 +1,28 @@
 #pragma once
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <string>
+using namespace std;
+
 class Account
 {
 private:
+	string username;
+	string password;
+	string firstName; //not sure
+	string lastName; //how to
+	string dateCreated; //collect
+	string gradeLevel; //this data
 public:
 	Account();
+	Account(string name, string pword);
+	string get_username();
+	string get_password();
+	int isUsernameAvailable(string name);
+	void createAccount(string name, string pword);
 	~Account();
 
 
 };
 
+#endif
