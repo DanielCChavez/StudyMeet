8f8177995d22e80fb3b28ccdc5631fab01b39952
Daniel Chavez <Chave071@cougars.csusm.edu>
2018-03-13 17:53:20 -0700

    Added member variables and get functions to session.h/.cpp

--- StudyMeet/Session.cpp
+++ StudyMeet/Session.cpp
@@ -10,3 +10,53 @@ Session::Session()
 Session::~Session()
 {
 }
+
+string Session::get_sessionID()
+{
+	return sessionID;
+}
+
+string Session::get_hostname()
+{
+	return hostName;
+}
+
+string Session::get_timestart()
+{
+	return timeStart;
+}
+
+int Session::get_numberofpeople()
+{
+	return numberOfPeople;
+}
+
+string Session::get_timeend()
+{
+	return timeEnd;
+}
+
+string Session::get_subject()
+{
+	return subject;
+}
+
+string Session::get_location()
+{
+	return location;
+}
+
+int Session::get_totalnumberofpeople()
+{
+	return totalNumberOfPeople;
+}
+
+list<Account> Session::get_listofpeople()
+{
+	return listOfPeople;
+}
+
+string Session::get_description()
+{
+	return description;
+}
--- StudyMeet/Session.h
+++ StudyMeet/Session.h
@@ -1,8 +1,37 @@
 #pragma once
+
+#include "Account.h"
+#include <list>
+
 class Session
 {
+
+private:
+	string sessionID;
+	string hostName;
+	string timeStart; 
+	int numberOfPeople;
+	string timeEnd;
+	string subject;
+	string location; 
+	int totalNumberOfPeople;
+	list<Account> listOfPeople;
+	string description;
+
 public:
 	Session();
 	~Session();
+
+	string get_sessionID();
+	string get_hostname();
+	string get_timestart();
+	int get_numberofpeople();
+	string get_timeend();
+	string get_subject();
+	string get_location();
+	int get_totalnumberofpeople();
+	list<Account> get_listofpeople();
+	string get_description();
+
 };
 
