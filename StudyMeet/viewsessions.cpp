7972463c8f7cac380843dd27aac6b3e21742bd42
Daniel Chavez <Chave071@cougars.csusm.edu>
2018-03-26 16:11:51 -0700

    added a mock session using a session object next will be conversion from one object to a list of session objects

--- StudyMeet/viewsessions.cpp
+++ StudyMeet/viewsessions.cpp
@@ -1,14 +1,36 @@
 #include "viewsessions.h"
-
+#include "Session.h"
 ViewSessions* ViewSessions::instance = NULL;
 
 ViewSessions::ViewSessions(QWidget *parent)
 	: QWidget(parent)
 {
 	QStringList titles;
+	Session test("123", 21, "1AM", 8, "3AM", "Computer Science", "Anywhere", 10, "i need help understanding an algorithm.");
 	titles << "Host" << "Subject" << "Time" << "Location";
+
+	QString time;
+	QString subject;
+	QString id;
+	QString location;
+	int row;
+	time = QString::fromStdString(test.get_timestart());
+	subject = QString::fromStdString(test.get_subject());
+	id = QString::number(test.get_hostId());
+	location = QString::fromStdString(test.get_location());
+
 	ui.setupUi(this);
 	ui.sessionTable->setHorizontalHeaderLabels(titles);
+	ui.sessionTable->insertRow(ui.sessionTable->rowCount());
+	row = ui.sessionTable->rowCount() - 1;
+
+	ui.sessionTable->setItem(row, 2, new QTableWidgetItem(time));
+
+	ui.sessionTable->setItem(row, 1, new QTableWidgetItem(subject));
+
+	ui.sessionTable->setItem(row, 0, new QTableWidgetItem(id));
+
+	ui.sessionTable->setItem(row, 3, new QTableWidgetItem(location));
 }
 
 
